4d8dbdefa8c4a048a05b58022dbd0489971f4b14
Israel Duarte <israeltduarte@gmail.com>
2023-09-19 20:28:58 -0300

    adding precificando back application

---
+++ precificando-back/src/test/java/br/isertech/com/precificando/precificandoback/PrecificandoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.isertech.com.precificando.precificandoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecificandoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
